aaa60a0542972e0d3e58cac432f597edeb6abda9
dalex <delphindidier0@gmail.com>
2020-04-25 13:42:09 -0400

    file migrations

---
+++ migrations/20200425170917-Users.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   return queryInterface.createTable('Users', {
+    id: {
+        type: Sequelize.INTEGER, 
+        autoIncrement: true, 
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING, 
+        unique: true
+    },
+    password: Sequelize.STRING
+
+})
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+
+
+  }
+};
