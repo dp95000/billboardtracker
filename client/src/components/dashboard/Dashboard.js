2ac9472aaddb8e764fcd1dbb7314b657051ea328
dp <davepollan@gmail.com>
2020-04-29 19:07:55 -0400

    Client Modal Form Complete

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import BillboardForm from './BillboardForm';
+import ClientForm from './ClientForm';
 
+{/* Function to Open Billboard Modal */}
 function modal(){
     var modal = document.getElementById("billboard_modal");
     modal.style.display = "block";
 }
 
-function close_modal(){
-    var close_modal = document.getElementById("billboard_modal");
-    close_modal.style.display = "none";
+{/* Function to Open Billboard Modal */}
+function modal2(){
+    var modal = document.getElementById("client_modal");
+    modal.style.display = "block";
 }
 
 export default function Dashboard() {
@@ -19,17 +22,17 @@ export default function Dashboard() {
                 
                 {/* Banner */}
                 <div className="container">
-                    
                         <div className="columns medium-12" id="banner">
                             <h1>Dashboard Login</h1>
                         </div>
-                    
                 </div>
 
                 <div className="row" id="dual-col">
                     {/* Billboard Locations */}
                     <div className="columns small-12 medium-12 large-6" id="sec1">
                         <h2>Billboard Locations</h2>
+
+                         {/* Billboard Modal Form*/}
                         <BillboardForm></BillboardForm>
                         
                         {/* Billboard Wrap*/}
@@ -38,25 +41,25 @@ export default function Dashboard() {
                                 <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="billboard_listing">
-                                <h3 className="list-title">Billboard Number</h3><button className="edit" data-open="BBmodal">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="billboard_listing">
-                                <h3 className="list-title">Billboard Number</h3><button className="edit" data-open="BBmodal">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="billboard_listing">
-                                <h3 className="list-title">Billboard Number</h3><button className="edit" data-open="BBmodal">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="billboard_listing">
-                                <h3 className="list-title">Billboard Number</h3><button className="edit" data-open="BBmodal">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="billboard_listing">
-                                <h3 className="list-title">Billboard Number</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="billboard_listing">
-                                <h3 className="list-title">Billboard Number</h3><button className="edit" data-open="BBmodal">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="billboard_listing">
-                                <h3 className="list-title">Billboard Number</h3><button className="edit" data-open="BBmodal">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Billboard Number</h3><button id="bbmodal" onClick={modal} className="edit">Edit</button><button className="delete">Delete</button>
                             </div>
                         </div>
 
@@ -64,75 +67,39 @@ export default function Dashboard() {
                     {/* Clients */}
                     <div className="columns small-12 medium-12 large-6" id="sec2">
                         <h2>Clients</h2>
+
+                        {/* Billboard Modal Form*/}
+                        <ClientForm></ClientForm>
                         
                          {/* Clients Wrap*/}
                         <div className="client_wrap">
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
                             <div className="client_listing">
-                                <h3 className="list-title">Client Name</h3><button className="edit">Edit</button><button className="delete">Delete</button>
+                                <h3 className="list-title">Client Name</h3><button id="clmodal" className="edit" onClick={modal2}>Edit</button><button className="delete">Delete</button>
                             </div>
+                           
                         </div>
 
-                        {/* Clients Form */}
-                        <form id="dashboard-form">
-                            <div className="container">
-
-                                {/* First Name */}
-                                <div className="columns small-12 medium-6 label">First Name:</div>
-                                <div className="columns small-12 medium-6"><input id="fname" type="text" /></div>
-
-                                {/* Last Name */}
-                                <div className="columns small-12 medium-6 label">Last Name:</div>
-                                <div className="columns small-12 medium-6"><input id="lname" type="text" /></div>
-
-                                {/* Business Name */}
-                                <div className="columns small-12 medium-6 label">Business Name:</div>
-                                <div className="columns small-12 medium-6"><input id="business_name" type="text" /></div>
-
-                                {/* Address */}
-                                <div className="columns small-12 medium-6 label">Billing Address:</div>
-                                <div className="columns small-12 medium-6"><input id="address" type="text" /></div>
-
-                                {/* Address */}
-                                <div className="columns small-12 medium-6 label">Phone:</div>
-                                <div className="columns small-12 medium-6"><input id="phone" type="tel" /></div>
-
-                                {/* Business Name */}
-                                <div className="columns small-12 medium-6 label">Signs Currently Rented<br></br> - Enter Sign IDs:</div>
-                                <div className="columns small-12 medium-6"><input id="business_name" type="text" /></div>
-                                
-                                {/* Business Name */}
-                                <div className="columns small-12 medium-6 label">Contact Term:</div>
-                                <div className="columns small-12 medium-6"><input id="contract_term" type="text" /></div>
-
-                                {/* Rent */}
-                                <div className="columns small-12 medium-6 label">Total Monthly Rent:</div>
-                                <div className="columns small-12 medium-6"><input id="renu" type="number" min="0" /></div>
-                            </div>
-
-                            <p>&nbsp;</p>
-                            <button type="submit" className="btn btn-default" id="sendform">Save</button>
-                        </form>
                     </div>
                 </div>
 
