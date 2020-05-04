137c612bae1388fa9f5c7223de60720646712126
tdj03001 <tyjenki@gmail.com>
2020-05-04 09:01:18 -0400

    saving local progress towards getting the collapsed header menu to expand on mobile

--- client/src/components/header/toggle.js
+++ client/src/components/header/toggle.js
@@ -8,43 +8,16 @@ import Header from "./header";
 
 // }
 
-//to fix issue that toggle adds style(hides) to nav
-// export function fixToggle() {
-//   window.resize(function () {
-//     if (window.innerWidth > 1024) {
-//       document.getElementById("menu").removeAttr("style");
-//     }
-//   })
-// }
 
-//icon animation
-// var topBar = document.getElementsByClassName("hamburger li:nth-child(1)"),
-//   middleBar = document.getElementsByClassName("hamburger li:nth-child(2)"),
-//   bottomBar = document.getElementsByClassName("hamburger li:nth-child(3)");
 
 export default function toggle() {
+  debugger
+  const coll = document.getElementsByClassName("collapsible");
 
-  // var topBar = document.getElementsByClassName("hamburger li:nth-child(1)"),
-  //   middleBar = document.getElementsByClassName("hamburger li:nth-child(2)"),
-  //   bottomBar = document.getElementsByClassName("hamburger li:nth-child(3)");
-
-  // if (middleBar.hasClass("rot-45deg")) {
-  //   topBar.removeClass("rot45deg");
-  //   middleBar.removeClass("rot-45deg");
-  //   bottomBar.removeClass("hidden");
-  // } else {
-  //   bottomBar.addClass("hidden");
-  //   topBar.addClass("rot45deg");
-  //   middleBar.addClass("rot-45deg");
-  // }
-
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
+  for (let i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
       this.classList.toggle("active");
-      var content = this.nextElementSibling;
+      const content = this.nextElementSibling;
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
