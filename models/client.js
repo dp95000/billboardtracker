808b827f4abf4b1cafadf460fe781b9dd85b73ea
tdj03001 <tyjenki@gmail.com>
2020-05-05 16:57:02 -0400

    removed models/client.js and models/post.js

--- models/client.js
+++
@@ -1,9 +0,0 @@
-//This is for update api routes and using ajax calls
-
-module.exports = (sequelize, DataTypes) => {
-    const Client = sequelize.define("Client", {
-        text: DataTypes.STRING,
-        complete: DataTypes.BOOLEAN
-    });
-    return Client;
-};
