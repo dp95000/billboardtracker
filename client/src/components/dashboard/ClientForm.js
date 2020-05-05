f3464f6c6b6b7b6fbceb34cd4f79feec687dbc4b
Ian Clark <ianclark226@gmail.com>
2020-05-05 13:26:23 -0400

    some functionality with models (#49)

--- client/src/components/dashboard/ClientForm.js
+++ client/src/components/dashboard/ClientForm.js
@@ -1,60 +1,121 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function close_modal2(){
-    var close_modal = document.getElementById("client_modal");
-    close_modal.style.display = "none";
+function close_modal2() {
+  var close_modal = document.getElementById("client_modal");
+  close_modal.style.display = "none";
 }
 
-export default function ClientForm() {
-    return(
-        <div>
-            {/* Client Modal Form*/}
-            <div id="client_modal" className="modal">
-             <div className="modal-content">
-             <span id="close_btn" onClick={close_modal2} className="close">&times;</span>
-                <h3>Add and Edit Customers</h3>
+export default function App() {
+  const [fields, setFields] = useState([]);
 
-                    {/* Clients Form */}
-                    <form id="dashboard-form">
-                            <div className="container">
-                                {/* First Name */}
-                                <div className="columns small-12 medium-6 label">First Name:</div>
-                                <div className="columns small-12 medium-6"><input id="fname" type="text" /></div>
+  const handleFieldChange = event => {
+    setFields({
+      ...fields,
+      [event.target.id]: event.target.value
+    });
+  };
 
-                                {/* Last Name */}
-                                <div className="columns small-12 medium-6 label">Last Name:</div>
-                                <div className="columns small-12 medium-6"><input id="lname" type="text" /></div>
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log("Submitted Form:", fields);
+  };
 
-                                {/* Business Name */}
-                                <div className="columns small-12 medium-6 label">Business Name:</div>
-                                <div className="columns small-12 medium-6"><input id="business_name" type="text" /></div>
+  console.log("fields updated:", fields);
 
-                                {/* Address */}
-                                <div className="columns small-12 medium-6 label">Billing Address:</div>
-                                <div className="columns small-12 medium-6"><input id="address" type="text" /></div>
+  return (
+    <div>
+      {/* Client Modal Form*/}
+      <div id="client_modal" className="modal">
+        <div className="modal-content">
+          <span id="close_btn" onClick={close_modal2} className="close">
+            &times;
+          </span>
+          <h1>Add and Edit Customers</h1>
+          {/* Clients Form */}
+          <form id="dashboard-form">
+            <div className="container">
+              {/* First Name */}
+              <div className="columns small-12 medium-6 label">First Name:</div>
+              <div className="columns small-12 medium-6">
+                <input id="fname" type="text" onChange={handleFieldChange} />
+              </div>
+              {/* Last Name */}
+              <div className="columns small-12 medium-6 label">Last Name:</div>
+              <div className="columns small-12 medium-6">
+                <input id="lname" type="text" onChange={handleFieldChange} />
+              </div>
+              {/* Business Name */}
+              <div className="columns small-12 medium-6 label">
+                Business Name:
+              </div>
+              <div className="columns small-12 medium-6">
+                <input
+                  id="business_name"
+                  type="text"
+                  onChange={handleFieldChange}
+                />
+              </div>
+              {/* Address */}
+              <div className="columns small-12 medium-6 label">
+                Billing Address:
+              </div>
+              <div className="columns small-12 medium-6">
+                <input id="address" type="text" onChange={handleFieldChange} />
+              </div>
+              {/* Address */}
+              <div className="columns small-12 medium-6 label">Phone:</div>
+              <div className="columns small-12 medium-6">
+                <input id="phone" type="tel" onChange={handleFieldChange} />
+              </div>
+              {/* Business Name */}
+              <div className="columns small-12 medium-6 label">
+                Signs Currently Rented
+                <br /> - Enter Sign IDs:
+              </div>
+              <div className="columns small-12 medium-6">
+                <input
+                  id="business_name"
+                  type="text"
+                  onChange={handleFieldChange}
+                />
+              </div>
 
-                                {/* Address */}
-                                <div className="columns small-12 medium-6 label">Phone:</div>
-                                <div className="columns small-12 medium-6"><input id="phone" type="tel" /></div>
-
-                                {/* Business Name */}
-                                <div className="columns small-12 medium-6 label">Signs Currently Rented<br></br> - Enter Sign IDs:</div>
-                                <div className="columns small-12 medium-6"><input id="business_name" type="text" /></div>
-                                
-                                {/* Business Name */}
-                                <div className="columns small-12 medium-6 label">Contact Term:</div>
-                                <div className="columns small-12 medium-6"><input id="contract_term" type="text" /></div>
-
-                                {/* Rent */}
-                                <div className="columns small-12 medium-6 label">Total Monthly Rent:</div>
-                                <div className="columns small-12 medium-6"><input id="renu" type="number" min="0" /></div>
-                            </div>
-
-                        <p>&nbsp;</p>
-                        <button type="submit" className="btn btn-default" id="sendform">Save</button>
-                </form>
-             </div>
-             </div>
+              {/* Business Name */}
+              <div className="columns small-12 medium-6 label">
+                Contact Term:
+              </div>
+              <div className="columns small-12 medium-6">
+                <input
+                  id="contract_term"
+                  type="text"
+                  onChange={handleFieldChange}
+                />
+              </div>
+              {/* Rent */}
+              <div className="columns small-12 medium-6 label">
+                Total Monthly Rent:
+              </div>
+              <div className="columns small-12 medium-6">
+                <input
+                  id="renu"
+                  type="number"
+                  min="0"
+                  onChange={handleFieldChange}
+                />
+              </div>
+            </div>
+            <p>&nbsp;</p>
+            <button
+              type="submit"
+              className="btn btn-default"
+              id="sendform"
+              onClick={handleSubmit}
+            >
+              Save
+            </button>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 
 if (config.use_env_variable) {
-    const sequelize = new Sequelize(process.env[config.use_env_variable]);
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
