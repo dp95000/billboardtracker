37f15a4d09f04ecb921af5843316d8dd44c3237b
Tyler <tyjenki@gmail.com>
2020-05-07 01:41:29 -0400

    Multi city function (#62)
    * deleted Demographics.js as an unused file
    
    * deleted utils folder (the one that had arcgis stuff in it, not Ian's)
    
    * added ajax calls and functions for the wilmington and trenton, removed big 'get demographics' button and gave each city its own smaller button
    
    * added an id to the city field in the demographics table in HomePage.js
    
    * added onClick listeners to each city's button

--- client/src/components/demographics/UserInputs.js
+++ client/src/components/demographics/UserInputs.js
@@ -1,26 +1,26 @@
 import React from "react";
-import getDemographics from "../homePage/jQueryDemographics";
+import { getDemographicsPhilly, getDemographicsWilmington, getDemographicsTrenton } from "../homePage/jQueryDemographics";
 
 export default function () {
   return (
     <div>
-      <p>Choose up to three cities for your demographics search</p>
+      <p><em>We have demographic data for three cities, and we're always expanding!</em></p>
 
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
-        <label className="form-check-label" htmlFor="defaultCheck1">Philadelphia, PA</label>
+        <label className="" htmlFor="defaultCheck1">Philadelphia, PA</label>
+        <button className="button" type="checkbox" value="" id="defaultCheck1" onClick={getDemographicsPhilly}>Get Demographics</button>
       </div>
 
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
-        <label className="form-check-label" htmlFor="defaultCheck1">Wilmington, DE</label>
+        <label className="" htmlFor="defaultCheck1">Wilmington, DE</label>
+        <button className="button" type="checkbox" value="" id="defaultCheck1" onClick={getDemographicsWilmington}>Get Demographics</button>
       </div>
 
       <div className="form-check">
-        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"></input>
-        <label className="form-check-label" htmlFor="defaultCheck1">Trenton, NJ</label>
+        <label className="" htmlFor="defaultCheck1">Trenton, NJ</label>
+        <button className="button" type="checkbox" value="" id="defaultCheck1" onClick={getDemographicsTrenton}>Get Demographics</button>
       </div>
-      <button id="demoBtn" type="button" className="button" onClick={getDemographics}>Get Demographics</button>
+      {/* <button id="demoBtn" type="button" className="button" onClick={getDemographics}>Get Demographics</button> */}
     </div>
   )
 }
