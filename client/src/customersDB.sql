d06db4bd3a2a8bc4861767d66cb6d0fb18837622
Ian Clark <ianclark226@gmail.com>
2020-06-10 19:38:45 -0400

    Index ic (#73)
    * some functionality with models
    
    * both forms are functional

--- client/src/customersDB.sql
+++ client/src/customersDB.sql
@@ -1,9 +1,11 @@
 create database customersDB;
 use customersDB;
 
+
 CREATE TABLE customers
 (
   first_name VARCHAR(100) NULL,
+
   last_name VARCHAR(100) NULL,
   business_name VARCHAR(100) NULL,
   bill_address VARCHAR(100) NULL,
@@ -14,4 +16,6 @@ CREATE TABLE customers
   monthly_rent INT NULL,
   delinquent_account VARCHAR(100) NULL,
   past_due_payment INT NULL
+
+
 );
--- models/index.js
+++ models/index.js
@@ -9,7 +9,9 @@ const db = {};
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 if (config.use_env_variable) {
+
      var sequelize = new Sequelize(process.env[config.use_env_variable]);
+
 } else {
      var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
