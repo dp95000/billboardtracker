3dac90efe39aa9561690aaa0489d84190f24c7df
tdj03001 <tyjenki@gmail.com>
2020-06-12 23:39:06 -0400

    Created a 'Create an account' page for new users

---
+++ client/src/components/login/Signup.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SignupFunction from '../loginfunction/SignupFunction';
+var passport = require("passport");
+
+export default function Signup() {
+  return (
+    <div>
+
+      <section id="content">
+        <div className="row">
+          <div className="columns medium-12" id="login">
+            <h2>Create an account</h2>
+            <form className="login" id="dashboard-form">
+              <input type="text" className="form-control" id="name-input" placeholder="Name" />
+              <input type="email" className="form-control" id="email-input" placeholder="Email" />
+              <input type="password" className="form-control" id="password-input" placeholder="Password" />
+              <button type="button" onClick={SignupFunction} id="sendform">Sign up</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
