1e913239a98b872cace1df4cf1facbfdf807abc8
tdj03001 <tyjenki@gmail.com>
2020-06-15 07:09:47 -0400

    synced mysql db with our app using db.sequelize.sync

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const cors = require("cors");
+const db = require("./models");
 
 app.use(cors());
 //const apiRoutes = require("./routes/api-routes");
@@ -14,9 +15,11 @@ app.use(cors());
 var passport = require("./config/passport");
 require('dotenv').config();
 
+// We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 require("./routes/api-routes.js")(app);
 
 if (process.env.NODE_ENV === "production") {
@@ -27,6 +30,8 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
