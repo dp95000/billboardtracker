1983f2624d8ff672bb4df70e0a5b19452a2b9368
Tyler <tyjenki@gmail.com>
2020-07-13 09:48:21 -0400

    Save to my sql - connected mySQL db so that newly created user accounts for the dashboard get written to the db (#78)
    * Created a 'Create an account' page for new users
    
    * Created the functions that allow users to sign up for an account. Still need to add code to save credentials to our DB
    
    * added error message to login page for when a user enters incorrect or incomplete credentials and clicks the login button. Stripped out some old console.logs too to clean up the file a bit.
    
    * Added a link to the sign up page
    
    * Added signup Route to Main.js
    
    * capitalized Account in Signup.js
    
    * creating connection to mysql db with new connectionjs file
    
    * updated seed data to include 'email' rather than 'user_name' in billboardDB_seed.sql
    
    * added 'drop database if exists' to the top of the customersDB.sql file
    
    * removed unused code
    
    * Added 'name' object to User model
    
    * added signup route to api-routes.js
    
    * synced mysql db with our app using db.sequelize.sync
    
    * 'prettier' saved some new spacing in isAuthenticated.js. No other changes were made
    
    * added a little spacing for easier reading to models/index.js
    
    * added 'app.use' for express to server.js
    
    * cleaned up whitespace in models/user.js
    
    * added functionality to write user's data (name, email, hashed password) to our mysql db
    
    * added a bunch of dependencies
    
    * updated package-lock.json
    
    * added password validation back into passport.js
    
    * Added 'require dotenv' to api-routes.js
    
    * No code changes - just formatting from 'prettier' in billboardDB_seed.sql
    
    * removed old comments and updated signupUser function in SignupFunction.js so new user credentials get written to MySQL
    
    * removed callback in signupUser function in SignupFunction.js and moved the 'window.location.replace' so it executes when the new user account is saved to MySQL.
    
    * added alert for invalid login so user knows they need to try again in LoginFunction.js
    
    * fixed '<<<<HEAD' leftover from merge resolution

--- models/index.js
+++ models/index.js
@@ -1,18 +1,19 @@
-//Sequilize the database and connection
 "use strict";
 
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(module.filename);
-const db = {};
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
+
 if (config.use_env_variable) {
      var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
      var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
 fs
      .readdirSync(__dirname)
      .filter(function (file) {
@@ -22,11 +23,14 @@ fs
           var model = sequelize["import"](path.join(__dirname, file));
           db[model.name] = model;
      });
+
 Object.keys(db).forEach(function (modelName) {
      if (db[modelName].associate) {
           db[modelName].associate(db);
      }
 });
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 module.exports = db;
