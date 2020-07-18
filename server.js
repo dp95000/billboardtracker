cf8a0fe4cdec5ebdc12ef3dc8f88879fb7c4d055
tdj03001 <tyjenki@gmail.com>
2020-07-18 17:39:26 -0400

    updated server.js with mysql db connection info

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const path = require("path");
 var session = require("express-session");
+var passport = require("./config/passport");
 const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 3001;
-const app = express();
+
 const cors = require("cors");
+const db = require("./models");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require('dotenv').config();
 
 app.use(cors());
 //const apiRoutes = require("./routes/api-routes");
 //app.use(apiRoutes);
 
-// Requiring passport as we've configured it
-var passport = require("./config/passport");
-require('dotenv').config();
-
+// We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
 require("./routes/api-routes.js")(app);
 
 if (process.env.NODE_ENV === "production") {
@@ -27,6 +33,8 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
