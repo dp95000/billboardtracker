78b724e9a19907a6a125495d17b7354c9131ff9c
tdj03001 <tyjenki@gmail.com>
2020-07-18 17:58:35 -0400

    Added 'name' to Userdata object in models/user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,3 @@
-
-
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var bcrypt = require("bcryptjs");
 // Creating our User model
@@ -19,7 +17,13 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false
         },
-    }, { timestamps: false });
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    }, {
+        timestamps: false
+    });
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
     User.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
