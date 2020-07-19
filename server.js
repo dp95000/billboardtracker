2af65f63156f1246637924d548a0054e9d14a4a1
tdj03001 <tyjenki@gmail.com>
2020-07-19 15:23:07 -0400

    added some previously commented code back into server.js to see what's breaking heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 3001;
 
 const cors = require("cors");
-// const db = require("./models");
+const db = require("./models");
 const app = express();
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
@@ -33,8 +33,8 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// db.sequelize.sync().then(function () {
-app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
-// });
